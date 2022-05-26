1368983978117af19975fe02d91c200cf264cccc
Fr1kadelbka <frikadelbka19@gmail.com>
2022-05-26 18:45:04 +0300

    'Edit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useLayoutEffect } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+    <ul>
+  <li className="li">
+      <p className="string">Test 1</p> 
+       <div className="button">
+         <button className="btn" alt="Code desktop">Edit</button> 
+        <button className="btn" alt="Code desktop">Delete</button> 
+       </div>
+  </li>
+  <li className="li">
+      <p className="string">Test 2</p>
+      <div className="button">
+         <button className="btn" >Edit</button> 
+        <button className="btn" >Delete</button> 
+       </div>
+  </li>
+</ul>
+</div>
+  ) 
+  
 }
 
 export default App;
